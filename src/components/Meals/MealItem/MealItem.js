6a000dfa4a08bbcb7e1449d5eadec4b9924e7459
shubham kumar <clboy768@gmail.com>
2023-04-27 03:02:26 +0530

    added meal item form and input and css

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MealItemForm from "./MealItemForm";
 import classes from './MealItem.module.css'
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -9,7 +10,7 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div><MealItemForm/></div>
     </li>
   );
 };

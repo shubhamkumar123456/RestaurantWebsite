52518143c01c6656cf8a5eab1eb74866e6b310a3
shubham kumar <clboy768@gmail.com>
2023-04-28 04:37:40 +0530

    added cart button close and open model

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 
 function App() {
+  const [cartIsShown, setcartIsShown] = useState(false);
+
+  const showCartHandler=()=>{
+    setcartIsShown(true);
+  }
+  const hideCartHandler=()=>{
+    setcartIsShown(false);
+  }
   return (
     <>
-    <Cart/>
-      <Header/>
+   {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import classes from './Modal.module.css'
 import ReactDom from 'react-dom'
 
 const Backdrop=props=>{
-    return <div className={classes.backdrop}></div>
+    return <div className={classes.backdrop} onClick={props.onClose}></div>
 }
 
 const ModalOverlay=props=>{
@@ -16,7 +16,7 @@ const portalElement=document.getElementById('overlays')
 const Modal = (props) => {
   return (
     <>
-     {ReactDom.createPortal(<Backdrop/>,portalElement)}
+     {ReactDom.createPortal(<Backdrop onClose={props.onClose}/>,portalElement)}
      {ReactDom.createPortal(<ModalOverlay>{props.children}</ModalOverlay>,portalElement)}
      {}
     </>

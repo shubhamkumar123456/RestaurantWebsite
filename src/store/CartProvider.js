b3341afc4497c614b1c9d41d3880bf8dced02a0e
shubham kumar <clboy768@gmail.com>
2023-05-01 21:16:59 +0530

    updated cart provider final

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,26 +8,53 @@ const defaultCartState={
 }
 
 const cartReducer =(state,action)=>{
+    // if(action.type==='ADD'){
+    //     const existingCartItemIndex=state.items.findIndex(item=>item.id===action.item.id);
+    //     const existingCartItem=state.items[existingCartItemIndex];
+    //     let updatedItems;
+        
+    //     if(existingCartItem){
+          
+    //        const updatedItem={
+    //             ...existingCartItem,
+    //             amount:existingCartItem.amount+action.item.amount
+    //         };
+    //         updatedItems=[...state.items];
+    //         updatedItems[existingCartItemIndex]=updatedItem
+    //     }else{
+         
+    //         updatedItems=state.items.concat(action.item)
+    //     }
+
+        
+    //     const updatedTotalAmount= state.totalAmount+action.item.price*action.item.amount
+    //     return {
+    //         items:updatedItems,
+    //         totalAmount:updatedTotalAmount
+    //     };
     if(action.type==='ADD'){
+        // const updatedItems= state.items.concat(action.item);
+        // const updatedTotalAmount= (state.totalAmount+action.item.price)*action.item.amount
         const existingCartItemIndex=state.items.findIndex(item=>item.id===action.item.id);
         const existingCartItem=state.items[existingCartItemIndex];
+   
         let updatedItems;
-        
+
         if(existingCartItem){
           
-           const updatedItem={
+         const  updatedItem={
                 ...existingCartItem,
-                amount:existingCartItem.amount+action.item.amount
+                amount:existingCartItem.amount+1
             };
             updatedItems=[...state.items];
             updatedItems[existingCartItemIndex]=updatedItem
         }else{
-         
+       
             updatedItems=state.items.concat(action.item)
         }
 
-        
-        const updatedTotalAmount= state.totalAmount+action.item.price*action.item.amount
+
+        const updatedTotalAmount= state.totalAmount+action.item.price
         return {
             items:updatedItems,
             totalAmount:updatedTotalAmount
